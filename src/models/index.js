a5eeadbee0f2181bdab509218b36e1722f0dbb55
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2023-02-11 17:18:19 +0530

    Authentication and authorization (#4)
    * added health-check endpoint
    
    * auth middlewares and refactoring
    
    * created routes
    
    * login signup flows
    
    * removed merge conflicts
    
    * merge conflicts resolution
    
    * changes

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
 const entitySchema = require('./entity/entity.schema');
+const profileSchema = require('./profile/profile.schema');
 
 exports.Entity = mongoose.model('Entity', entitySchema);
+exports.Profile = mongoose.model('Profile', profileSchema);
